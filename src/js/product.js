4a004209122ede035ecd73383f7b016c07a76da8
enochpalayar <epalayar@byupathway.edu>
2026-07-09 13:43:58 +0800

    Update Team Activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -3,24 +3,24 @@ import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam('product');
+const productId = getParam("product");
 
 //console.log(dataSource.findProductById(productId));
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
 //function addProductToCart(product) {
-  //const cartItems = getLocalStorage("so-cart") || [];
- //cartItems.push(product);
-  //setLocalStorage("so-cart", cartItems);
+//const cartItems = getLocalStorage("so-cart") || [];
+//cartItems.push(product);
+//setLocalStorage("so-cart", cartItems);
 //}
 // add to cart button event handler
 //async function addToCartHandler(e) {
-  //const product = await dataSource.findProductById(e.target.dataset.id);
-  //addProductToCart(product);
+//const product = await dataSource.findProductById(e.target.dataset.id);
+//addProductToCart(product);
 //}
 
 // add listener to Add to Cart button
 //document
-  //.getElementById("addToCart")
-  //.addEventListener("click", addToCartHandler);
+//.getElementById("addToCart")
+//.addEventListener("click", addToCartHandler);
